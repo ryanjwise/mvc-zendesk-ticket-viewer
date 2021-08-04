dec62ea3aa0359e44901ecb4e676123b34d530ae
Ryan Wise <ryan.jeffwise@gmail.com>
2021-08-04 14:31:09 +1000

    Create application file

---
+++ lib/launcher.rb
@@ -0,0 +1,14 @@
+##### Dependencies #####
+
+##### File imports #####
+### Models ###
+### Views ###
+### Controllers ###
+# Main Application Controller
+# require_relative 'controllers/app'
+
+# Handle ARGS
+
+# Core Loop Start
+app = Application.new
+app.launch
---
+++ run.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+# install project dependencies if required
+bundle install
+
+# launch project
+ruby ./lib/launcher.rb
