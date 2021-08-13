45faf5b82295e30a8ccd47d21d4355585b73dcf2
Ryan Wise <ryan.jeffwise@gmail.com>
2021-08-13 17:30:42 +1000

    remove debug statements from prompt

--- lib/views/prompt.rb
+++ lib/views/prompt.rb
@@ -3,8 +3,6 @@ module Prompt
   @prompt = TTY::Prompt.new
 
   def self.menu(menu_items)
-    puts "You've hit the menu print method."
-    puts 'it has the following items passed to it:'
     @prompt.select('What would you like to do?', menu_items)
   end
 end
